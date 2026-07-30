c5ea360315f00f1f4a5ee45d4ffce929de271178
WangZhanze <wangzhanze@huoban.ai>
2026-07-30 19:37:39 +0800

    feat: add design token system and base styles

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,10 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
+import "./styles/tokens.css";
+import "./styles/base.css";
+import "./styles/app.css";
+import "./styles/views.css";
 import "./App.css";
 
 createRoot(document.getElementById("root")!).render(
